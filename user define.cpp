859885d052fe1f2b7ac5bbb59a16290e9da68373
vadla071 <112850456+vadla071@users.noreply.github.com>
2022-09-05 14:08:48 +0530

    Add files via upload

---
+++ user define.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+	int a,b,c;
+	{
+		std::cout<<"enter the value of a\n";
+		std::cin>>a;
+		std::cout<<"enter the value of b\n";
+		std::cin>>b;
+		c=a+b;
+		std::cout<<c;
+	}
+	return 0;
+}
