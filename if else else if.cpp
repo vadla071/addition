4d7f4a69268cb62ae2ebcf27ed2cbd0dad64f70e
vadla071 <112850456+vadla071@users.noreply.github.com>
2022-09-05 13:25:52 +0530

    Add files via upload

---
+++ if else else if.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main()
+{
+	int a,b;
+	a=5;
+	b=6;
+	if(a>b)
+	{
+	    std::cout<<"a is greater";
+	}
+	else if(b>a)
+	{
+	    std::cout<<"b is greater";
+	}
+	return 0;
+}
+}
